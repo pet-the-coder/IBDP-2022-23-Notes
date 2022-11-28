fdbadddc804767d3161ad301c98c51d8d27c1f34
Peter Zhang <ultraswordkirbypink@gmail.com>
2022-11-27 22:40:49 -0500

    math ia lets go

--- HL Math/Math IA/cubicregression.py
+++ HL Math/Math IA/cubicregression.py
@@ -2,13 +2,15 @@
 import math
 import numpy as np
 
+from typing import List, Union
 
 def _5dec(num):
     return int(num * 100000) / 100000
 
+# ---------------------------------------------- #
+
 
 # flipping matrices
-
 # goal is to calculate for first matrix
 # right now, all I need to do is collect first 
 # 100 values, take first 2 and last 2, output 
@@ -39,14 +41,18 @@ print(np.shape(XT))
 for i in range(4):
     print("&".join([str(_5dec(XT[i][0])), str(_5dec(XT[i][1])), "...", str(_5dec(XT[i][-2])), str(_5dec(XT[i][-1]))]) + "\\\\")
 
-
 # # next solve for XT*X
-# XT_X = np.matmul(XT, X)
-# print(XT_X)
-# iXT_X = np.linalg.inv(XT_X)
+XT_X = np.matmul(XT, X)
+print(np.shape(XT_X))
+for i in range(4):
+    print("&".join([str(_5dec(XT_X[i][0])), str(_5dec(XT_X[i][1])), str(_5dec(XT_X[i][-2])), str(_5dec(XT_X[i][-1]))]) + "\\\\")
 
-# coef = np.matmul(np.matmul(iXT_X, XT), Y)
+# find inverse
+iXT_X = np.linalg.inv(XT_X)
+print(np.shape(iXT_X))
+for i in range(4):
+    print("&".join([str(_5dec(iXT_X[i][0])), str(_5dec(iXT_X[i][1])), str(_5dec(iXT_X[i][-2])), str(_5dec(iXT_X[i][-1]))]) + "\\\\")
 
-# print(coef)
+coef = np.matmul(np.matmul(iXT_X, XT), Y)
 
-
+print(coef)
