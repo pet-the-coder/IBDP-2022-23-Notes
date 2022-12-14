89d0efe6b140dc2ffd9b3e63d147bcce82c542d5
Peter Zhang <ultraswordkirbypink@gmail.com>
2022-12-13 21:58:43 -0500

    physics lab+ chemistry

--- HL Physics/labs/Temperature Lab/find_volume.py
+++ HL Physics/labs/Temperature Lab/find_volume.py
@@ -16,23 +16,32 @@ for i in POINTS:
     LEFT.append(float(j[0]))
     RIGHT.append(float(j[1]))
 
+# print(LEFT, RIGHT)
+
 # find volume
+n = 50 / (6.02*10**23)
+R = 8.31
 def volume(p, t):
-    return (50/6.02/(10**23) * 8.31 * p) / t
-
-# find volume values
-VOLUME = [volume(i, j) for i, j in zip(LEFT, RIGHT)]
-
-# avg volume
-VAVG = sum(VOLUME) / len(VOLUME)
-print("Average Volume:", VAVG)
+    v = (n * R * p) / t
+    print(p, t, v)
+    return v
 
 # uncertainty
 def find_percent_unc(val, uncer):
     return uncer / val * 100
 
-UVP = 0.01
+# find volume values
+VOLUME = [volume(j, i) for i, j in zip(LEFT, RIGHT)]
+
+UVP = 0.03
 UPLIST = [find_percent_unc(v, UVP) for v in RIGHT]
 
+# for i in range(len(UPLIST)):
+#     print(f"{VOLUME[i]} +- {UPLIST[i] * VOLUME[i]/100}")
+
+# avg volume
+VAVG = sum(VOLUME) / len(VOLUME)
+print("Average Volume:", VAVG)
+print(sum(UPLIST) / 100)
 print("ERROR:", sum(UPLIST) / 100 * VAVG)
 
